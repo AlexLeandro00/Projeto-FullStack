68b93cb2fcfcea824658ee4cc4ade4399eba0ca1
AlexLeandro00 <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>
2026-08-13 20:27:11 -0300

    Adiciona endpoint POST para criar recursos (Dia 5/6)

--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ from sqlalchemy.orm import sessionmaker
 from database import engine
 from models import Recurso
 from logica import calcular_dias_restantes
+from pydantic import BaseModel
+from datetime import date 
 
 app = FastAPI()
+class RecursoCreate(BaseModel):
+    nome: str
+    intervalo_medio_dias: int
+    data_ultima_compra: date
+    id_contato: int
+
+
+
 Session = sessionmaker(bind=engine)
 
 @app.get("/recursos")
@@ -26,3 +36,21 @@ def detalhe_recurso(nome: str):
 
     dias = calcular_dias_restantes(recurso)
     return {"nome": recurso.nome, "dias_restantes": dias}
+
+
+@app.post("/recursos")
+def criar_recurso(recurso: RecursoCreate):
+    session = Session()
+    novo_recurso = Recurso(
+        nome=recurso.nome,
+        intervalo_medio_dias=recurso.intervalo_medio_dias,
+        data_ultima_compra=recurso.data_ultima_compra,
+        id_contato=recurso.id_contato
+    )
+
+    session.add(novo_recurso)
+    session.commit()
+    session.refresh(novo_recurso)
+    session.close()
+
+    return{"Id": novo_recurso.id, "Nome": novo_recurso.nome, "mensagem": "Recurso criado com sucesso!"  }
